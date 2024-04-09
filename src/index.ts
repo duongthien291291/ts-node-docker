4e310c750a656837c44cd230d2dfd08db71c7da3
thong.duong@nois.vn <thong.duong@nois.vn>
2024-04-09 15:56:44 +0700

    update port

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import express, { Request, Response } from "express";
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript E1xpress!112 asdfasdf ");
+  res.send("Hello, TypeScript E1xpress!112 asdfasdf");
 });
 
 app.listen(port, () => {
